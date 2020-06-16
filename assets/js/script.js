bf333339aca8d56a7017fc0228f028c464b852c8
essennejaye <sjeffers02@gmail.com>
2020-06-16 14:54:41 -0700

    created tasks list with unique id's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -25,6 +27,10 @@ var createTaskEl = function (taskDataObj) {
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+    
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     // give it a class name
@@ -35,5 +41,7 @@ var createTaskEl = function (taskDataObj) {
     listItemEl.appendChild(taskInfoEl);
     // append entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+    // increase task counter for next unique id
+    taskIdCounter++;
 }
 formEl.addEventListener("submit", taskFormHandler);
